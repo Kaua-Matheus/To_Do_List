45fc3f83b38c6b9a1408b82d79b0055ced9f67aa
Kaua-Matheus <kauamatheussilva06@gmail.com>
2026-09-25 20:52:29 -0300

    feat: Added differents crud operations in user

--- backend/repositories/user_repository.py
+++ backend/repositories/user_repository.py
@@ -4,15 +4,45 @@ from models.user import User
 
 class UserRepository:
 
-    async def create_user(self, db_session: AsyncSession, user_data: dict):
+    # View Operations
+
+    async def get_user_by_id(self, db: AsyncSession, user_id: int) -> User:
+        try:
+            return await db.get(User, user_id)
+            
+        except Exception as e:
+            raise e
+
+
+    # Create Operations
+
+    async def create_user(self, db: AsyncSession, user_data: dict) -> User:
         try:
             new_user = User(**user_data)
-            db_session.add(new_user)
-            await db_session.commit()
-            await db_session.refresh(new_user)
+            await db.add(new_user)
+            await db.commit()
+            await db.refresh(new_user)
 
-            return new_user.__dict__
+            # Test
+            # return new_user.__dict__
+            return new_user
 
         except Exception as e:
-            await db_session.rollback()
+            await db.rollback()
+            raise e
+        
+    # Delete Operations
+
+    async def delete_user(self, db: AsyncSession, user_id: int) -> bool:
+        try:
+
+            user = await db.get(User, user_id)
+            if user is None:
+                return False
+            
+            await db.delete(user)
+            await db.commit()
+            return True
+
+        except Exception as e:
             raise e
